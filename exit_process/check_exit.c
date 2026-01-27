6fc7f45e79900be9610d5c5696a3008c2768e514
Nuno Araújo <gurito.araujo@gmail.com>
2026-01-27 13:59:39 +0000

    fixed exit and did optional for echo

--- exit_process/check_exit.c
+++ exit_process/check_exit.c
@@ -12,19 +12,26 @@
 
 #include "../minishell.h"
 
-int check_exit (char *prompt)
+int check_exit(char *prompt)
 {
-	char *trimmed;
-	int	i;
+    char	**split;
+    int		exit_code;
+    int		i;
 
-	i = 0;
-	trimmed = ft_strtrim(prompt, " ");
-	while (trimmed[i])
-		i++;
-	trimmed[i] = '\0';
-	if (ft_strncmp(trimmed, "exit", 4) == 0)
-		if (trimmed[4] == ' ' || trimmed[4] == '\0')
-			return (ft_printf("Tem exit\n"), free(trimmed), 0);
-	free(trimmed);
-	return (1);
+    split = ft_split(prompt, ' ');
+    if (!split || ft_strcmp(split[0], "exit") != 0)
+        return (free_split(split), 1);
+    
+    exit_code = 0;
+    if (split[1])
+    {
+        i = 0;
+        while (split[1][i] && ft_isdigit(split[1][i]))
+            i++;
+        if (split[1][i] != '\0')
+            return (ft_printf("exit: invalid argument\n"), free_split(split), 1);
+        exit_code = ft_atoi(split[1]);
+    }
+    free_split(split);
+    exit(exit_code);  // Sai já aqui
 }
