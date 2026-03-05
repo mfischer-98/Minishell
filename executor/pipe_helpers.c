caa04bca40501cec34307ebd1033fd87d2c1d06f
Nuno Araújo <gurito.araujo@gmail.com>
2026-03-05 20:16:45 +0000

    fixed 2 errors and divided and organized all

---
+++ executor/pipe_helpers.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipe_helpers.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mefische <mefische@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/05 00:00:00 by mefische          #+#    #+#             */
+/*   Updated: 2026/03/05 00:00:00 by mefische         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+static void
+handle_child(int has_next, int pipefd[], char **cmd,
+        t_mshell_data *data, t_tokens *segment)
+{
+    if (has_next)
+        dup2(pipefd[1], STDOUT_FILENO);
+    execute_external_command(cmd, data, segment);
+}
+
+static int
+handle_parent(int has_next, int pipefd[], int saved,
+        t_mshell_data *data, pid_t pid)
+{
+    int status;
+    if (has_next)
+        dup2(pipefd[0], STDIN_FILENO);
+    else
+    {
+        dup2(saved, STDIN_FILENO);
+        waitpid(pid, &status, 0);
+        close(saved);
+        if (WIFEXITED(status))
+            data->exit_status = WEXITSTATUS(status);
+        else if (WIFSIGNALED(status))
+            data->exit_status = 128 + WTERMSIG(status);
+        return (0);
+    }
+    close(pipefd[1]);
+    waitpid(pid, &status, 0);
+    return (1);
+}
+
+static int
+fail_saved(int saved)
+{
+    dup2(saved, STDIN_FILENO);
+    close(saved);
+    return (0);
+}
+
+static int
+process_segment(t_mshell_data *data, t_tokens **tokens, int saved)
+{
+    t_tokens *next = *tokens;
+    while (next && next->type != NODE_PIPE)
+        next = next->next;
+    char **cmd = build_command(tokens);
+    if (!cmd)
+        return (fail_saved(saved));
+    int pipefd[2];
+    if (next)
+        pipe(pipefd);
+    pid_t pid = fork();
+    if (pid == 0)
+        handle_child(next != NULL, pipefd, cmd, data, *tokens);
+    else if (pid > 0)
+    {
+        if (!handle_parent(next != NULL, pipefd, saved, data, pid))
+            return (0);
+    }
+    else
+        return (fail_saved(saved));
+    if (next)
+        *tokens = next->next;
+    return (1);
+}
+
+void	execute_piped_commands(t_mshell_data *data, t_tokens *tokens)
+{
+    int saved = dup(STDIN_FILENO);
+    if (saved == -1)
+        return;
+    while (tokens && process_segment(data, &tokens, saved))
+        ;
+}
